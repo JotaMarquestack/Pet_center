7bfcae2e9b475edcafb97557ac54d75a4af275e1
Victor Emanuel dos Santos Brito <victorfaculdade40@gmail.com>
2026-06-04 08:05:16 -0300

    feat/Agendamento: Modulando conexão com database.spy catastro na main.py

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
-import os
-import dns.resolver  
-from dotenv import load_dotenv
-from pymongo import MongoClient
+from database import conectar_banco
 
-dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
-dns.resolver.default_resolver.nameservers = ['8.8.8.8']
+# Conexão com o MongoDB
 
-load_dotenv()
-url_conexao = os.getenv("URL_BANCO")
 
-try:
-    print("Tentando conectar ao banco de dados...")
-    cliente = MongoClient(url_conexao)
-    
-    db = cliente["clinica_pet"]
-    
-    cliente.admin.command('ping')
-    print("Sucesso! Conexão estabelecida com o MongoDB Atlas.")
-    
-except Exception as erro:
-    print("Erro ao conectar:")
-    print(erro)
+db = conectar_banco()
+col = db.get_collection("clientes")
+
+# Cadastro Básico e inserção de dados do Tutor e Pet no MongoDB
+if db is not None:
+    print("\n--- INICIANDO NOVO CADASTRO ---")
+    tutor_nome = input("Nome do tutor: ")
+    tutor_cpf = input("CPF do tutor: ")
+    tutor_telefone = input("Telefone do tutor: ")
+    tutor_email = input("Email do tutor: ")
+
+    pet_nome = input("Nome do Pet: ")
+    pet_especie = input("Espécie do Pet: ")
+    pet_raca = input("Raça do Pet: ")
+    pet_sexo = input("Sexo do Pet (M/F): ")
+    pet_peso = float(input("Peso do Pet (em kg, use ponto, ex: 5.3): "))
+
+    pet_castrado_resposta = input("Seu pet já é castrado? (S/N): ").strip().upper()
+    pet_castrado = True if pet_castrado_resposta == 'S' else False
+
+    pet_observacao = input("Seu pet possui alguma alergia ou observação?: ")
+
+    dados = {
+        "tutor": {
+            "nome": tutor_nome,
+            "cpf": tutor_cpf,
+            "telefone": tutor_telefone,
+            "email": tutor_email
+        },
+        "pet": {
+            "nome": pet_nome,
+            "especie": pet_especie,
+            "raca": pet_raca,
+            "sexo": pet_sexo,
+            "peso_kg": pet_peso,
+            "castrado": pet_castrado,
+            "observacoes": pet_observacao
+        }
+    }
+    print("\n Estrutura montada com sucesso!")
+
+col.insert_one(dados)
