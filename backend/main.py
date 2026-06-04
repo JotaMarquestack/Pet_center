53e5ed9cfe3958afdbffda4b8e6daea1fc468eed
Victor Emanuel dos Santos Brito <victorfaculdade40@gmail.com>
2026-06-04 18:30:57 -0300

    feat/backend-cadastro: Cadastro do Pet adicionado

--- backend/main.py
+++ backend/main.py
@@ -35,3 +35,32 @@ def login_tutor(email, senha_digitada):
         return f"Login aprovado! Acesso liberado para {cliente['nome']}."
     else:   
         return f"Erro: Email ou senha incorretos."
+
+def cadastrar_pet(email_tutor, nome_pet, especie, raca, peso, castrado, sexo, observacoes):
+    db = conectar_banco()
+    if db is None:
+        return "Erro de conexão com o banco."
+    
+    col = db.get_collection("clientes")
+    tutor = col.find_one({"email":email_tutor})
+
+    if not tutor:
+        return f"Erro, tutor não encontrado. Não é possível cadastrar o pet"
+    
+    
+    novo_pet = {
+        "nome": nome_pet,
+        "especie": especie,
+        "raca": raca,
+        "peso": float(peso),
+        "castrado": castrado,
+        "sexo": sexo,
+        "observacoes": observacoes
+    }
+
+    col.update_one(
+        {"email":email_tutor},
+        {"$push": {"pets": novo_pet}}
+    )
+
+    return f"Sucesso! O pet {nome_pet} foi adicionado à ficha de {tutor['nome']}"
