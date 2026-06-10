6c180fa32b874686a74f8c14dd5b31ab3a7d04c7
Victor Emanuel dos Santos Brito <victorfaculdade40@gmail.com>
2026-06-10 17:46:48 -0300

    organizando dev

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from login_cadastro import cadastrar_tutor, login_tutor
+from agendamento import agendar_consulta
 import os 
 from dotenv import load_dotenv
 from datetime import timedelta
@@ -82,6 +83,28 @@ def cadastro_page():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/agendar_pelo_site', methods=['POST'])
+def receber_agendamento_do_site():
+
+    e_tutor = request.form.get("email_tutor")
+    if e_tutor:
+        e_tutor = e_tutor.strip()
+
+    i_pet = request.form.get("id_pet")
+    if i_pet:
+        i_pet = i_pet.strip()
+
+    servico = request.form.get("servico")
+    data_c = request.form.get("data")
+    hora_c = request.form.get("hora")
+    obs = request.form.get("observacoes")
+
+    motivo_completo = f"{servico} - {obs}"
+    
+    resposta_do_banco = agendar_consulta(e_tutor, i_pet, data_c, hora_c, motivo_completo)
+    return(resposta_do_banco)
 
 if __name__ == '__main__':
     app.run(debug=True)
