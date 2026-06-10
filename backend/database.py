d8152b57b357ba6282364ed43ce0361110ae983c
BigPeu <henrique491000@gmail.com>
2026-06-10 20:19:38 -0300

    apenas ajeitando as conexoes

--- backend/database.py
+++ backend/database.py
@@ -1,23 +1,21 @@
-#Arquivo para o teste de conexão com o MongoDB
 import os
-import dns.resolver  
+import certifi  # <-- Importa o pacote de certificados instalado
 from dotenv import load_dotenv
 from pymongo import MongoClient
-
-dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
-dns.resolver.default_resolver.nameservers = ['8.8.8.8']
 
 def conectar_banco():
     load_dotenv()
     url_conexao = os.getenv("URL_BANCO")
-
-    try:
-        cliente = MongoClient(url_conexao)
-        db = cliente.get_database("clinica_pet")
-
-        cliente.admin.command('ping')
-        return db
     
+    try:
+        # Passamos o tlsCAFile para o Mongo usar os certificados seguros do certifi
+        cliente = MongoClient(url_conexao, tlsCAFile=certifi.where())
+        db = cliente.get_database("clinica_pet")
+        
+        cliente.admin.command('ping')
+        print("Conexão com o MongoDB estabelecida com sucesso!")
+        return db
+        
     except Exception as erro:
         print(f"Erro ao conectar com o banco: {erro}")
         return None
